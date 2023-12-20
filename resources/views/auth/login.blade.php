ccc473f648e42b7049f2e18705a59b502370f281
Gautier <gautier.fournet@gmail.com>
2023-12-20 22:39:26 +0100

    Modif HTML login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,34 @@
 @extends("templates.app")
 
+@vite(['resources/css/login.css'])
+
 @section('content')
 
-    <form action="{{route("login")}}" method="post">
-        @csrf
-        <input type="email" name="email" required placeholder="Email" /><br />
-        <input type="password" name="password" required placeholder="password" /><br />
-        Remember me<input type="checkbox" name="remember"   /><br />
-        <input type="submit" /><br />
-    </form>
+    <div class="flex-login">
+        <img src="images/imgconnect.png" class="bloc_rouge" alt=""> 
+        
+        <div>
+            <h1 class="h1login">⭐️ Enfin, ça faisait longtemps !</h1>
+            <p>Rejoignez la plus grande app de stockage de photos de l’IUT de Lens</p>
+            
+            <form action="{{route("login")}}" method="post">
+                @csrf
+                <fieldset>
+                    <legend>Email :</legend>
+                    <input type="email" name="email" required placeholder="Email" /><br />
+                </fieldset>
+                <fieldset>
+                    <legend>Mot de passe :</legend>
+                    <input type="password" name="password" required placeholder="password" /><br />
+                </fieldset>
+                <!-- Remember me<input type="checkbox" name="remember"   /><br /> -->
+                <input type="submit" value="Se Connecter" class="submitlogin"/><br />
+                <div class="no_count">
+                    Vous n'avez pas de compte ? 
+                    <a href="{{route("register")}}" class="pasdecompte">Inscrivez-vous</a>
+                </div>
+            </form>
+        </div>
+    </div>
+
 @endsection
