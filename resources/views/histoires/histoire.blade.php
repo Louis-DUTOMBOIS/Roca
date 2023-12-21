19190c406c2cdaac1453aadfc186fc6b961a7987
Gautier <gautier.fournet@gmail.com>
2023-12-21 07:21:36 +0100

    Modif html detail histoire

--- resources/views/histoires/histoire.blade.php
+++ resources/views/histoires/histoire.blade.php
@@ -1,56 +1,55 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Détails de la scène</title>
-    @vite(['resources/css/accueil.css', 'resources/css/sceneDetails.css', 'resources/css/footer.css'])
-    <!-- Assurez-vous d'inclure tout lien vers des styles CSS, des bibliothèques ou des scripts nécessaires -->
-</head>
-<body>
-<div class="detail">
-    <h1>Détails de la scène</h1>
+@extends("templates.app")
 
-    <h2>{{ $histoire->titre }}</h2>
-    <p>Description : {{ $histoire->pitch }}</p>
+@section('css')
+    @vite('resources/css/detailhistoire.css')
+@endsection
 
-    <img src="{{$histoire->photo}}" alt="Image calculée">
+@section('content')
+    <div class="detail">
+        <h1>Détails de la scène</h1>
 
-    <p>Nombre de lectures total terminé : {{ $histoire->terminees->sum('pivot.nombre') }}</p>
-
-    <p>Nombre d'avis : {{ $histoire->avis()->count()  }}</p>
-
-    <p>Auteur de la scène : {{ $histoire->user->name }}</p>
-
-    <p>Nombre de chapitres : {{ $histoire->chapitres->count() }}</p>
-
-    <form method="POST" action="{{ route('startReading') }}">
-        @csrf
-        <input type="hidden" name="histoire_id" value="{{ $histoire->id }}">
-        <button type="submit">Commencer la lecture</button>
-    </form>
-
-    @auth
-        <form method="POST" action="{{ route('ajouterAvis') }}">
+        <h2>{{ $histoire->titre }}</h2>
+        <img class="histoire-img" src="{{$histoire->photo}}" alt="Image calculée">
+        <form method="POST" action="{{ route('startReading') }}">
             @csrf
             <input type="hidden" name="histoire_id" value="{{ $histoire->id }}">
-            <textarea name="contenu" placeholder="Votre commentaire ici"></textarea>
-            <button type="submit">Ajouter un commentaire</button>
+            <button type="submit">Commencer la lecture</button>
         </form>
-    @endauth
+
+        <p>Description : {{ $histoire->pitch }}</p>
 
 
-    <h3>Avis sur cette histoire :</h3>
-    <ul>
-        @foreach($histoire->avis as $avis)
-            <li>
-                <p>Utilisateur : <button><a href="{{ route('user.show', $avis->user->id) }}">{{ $avis->user->name }}</a></button></p>
+        <p>Nombre de lectures total terminé : {{ $histoire->terminees->sum('pivot.nombre') }}</p>
 
-                <p>Commentaire : {{ $avis->contenu }}</p>
-                <!-- Ajoutez d'autres détails de l'avis au besoin -->
-            </li>
-        @endforeach
-    </ul>
+        <p>Nombre d'avis : {{ $histoire->avis()->count()  }}</p>
 
-</div>
+        <p>Auteur de la scène : {{ $histoire->user->name }}</p>
 
-</body>
-</html>
+        <p>Nombre de chapitres : {{ $histoire->chapitres->count() }}</p>
+
+
+        @auth
+            <form method="POST" action="{{ route('ajouterAvis') }}">
+                @csrf
+                <input type="hidden" name="histoire_id" value="{{ $histoire->id }}">
+                <textarea name="contenu" placeholder="Votre commentaire ici"></textarea>
+                <button type="submit">Ajouter un commentaire</button>
+            </form>
+        @endauth
+
+
+        <h3>Avis sur cette histoire :</h3>
+        <ul>
+            @foreach($histoire->avis as $avis)
+                <li>
+                    <p>Utilisateur : <button><a href="{{ route('user.show', $avis->user->id) }}">{{ $avis->user->name }}</a></button></p>
+
+                    <p>Commentaire : {{ $avis->contenu }}</p>
+                    <!-- Ajoutez d'autres détails de l'avis au besoin -->
+                </li>
+            @endforeach
+        </ul>
+
+    </div>
+@endsection
+
