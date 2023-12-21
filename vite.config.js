e90e1acae9f8d109627f9c6b264e6fa03a17f5e7
Dutombois Louis <louis_dutombois@ens.univ-artois.fr>
2023-12-21 15:01:57 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,10 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/normalize.css', 'resources/css/app.css', 'resources/js/app.js',
-<<<<<<< HEAD
-                'resources/css/test-vite.css', 'resources/css/equipe.css', 'resources/js/test-vite.js', 'resources/css/welcome.css',],
-=======
 
                 'https://code.jquery.com/jquery-3.6.4.min.js',
                 'resources/css/test-vite.css', 'resources/css/welcome.css', 'resources/css/equipe.css','resources/css/register.css', 'resources/css/login.css','resources/css/creeHistoire.css','resources/css/detailhistoire.css', 'resources/js/test-vite.js','resources/css/chapitreDetails.css'],
 
->>>>>>> 63e78d4d3f85232365f7c8443d5b98e976a26258
             refresh: true,
         }),
     ],
